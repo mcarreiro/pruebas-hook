bcbdaf4228749bf85caa75660aca3cfd3abe9b4b
Martin Carreiro <martin.carreiro@mercadolibre.com>
2017-09-08 15:22:44 -0300

    a

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ var execSync = require('child_process').execSync;
 execSync("git clone https://github.com/mcarreiro/pruebas-hook").toString()
 
 app.get('/webhook', function (req, res) {
-  //var result = execSync("git rev-list --left-right --count origin/master...origin/develop").toString()
-  //var behind = parseInt(result.split(" ")[0]) > 0;
-  var result = execSync("git --git-dir=pruebas-hook/.git branch -v").toString()
-  result += execSync("git --git-dir=pruebas-hook/.git remote -v ").toString()
+  var result = execSync("git --git-dir=pruebas-hook/.git rev-list --left-right --count origin/master...origin/develop").toString()
+  var behind = parseInt(result.split(" ")[0]) > 0;
   res.send(result)
 });
 
