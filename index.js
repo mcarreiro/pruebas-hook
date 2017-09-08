0c09420cc4a5f322bd737ff4f6bd9e78e5e4c919
Martin Carreiro <martin.carreiro@mercadolibre.com>
2017-09-08 15:23:28 -0300

    a

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ execSync("git clone https://github.com/mcarreiro/pruebas-hook").toString()
 app.get('/webhook', function (req, res) {
   var result = execSync("git --git-dir=pruebas-hook/.git rev-list --left-right --count origin/master...origin/develop").toString()
   var behind = parseInt(result.split(" ")[0]) > 0;
-  res.send(result)
+  res.send("Behind" + result)
 });
 
 app.listen(process.env.PORT || 3000, function () {
