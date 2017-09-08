7516b2cf55ca57b1ab652c39ab698f34c54a4c56
Martin Carreiro <martin.carreiro@mercadolibre.com>
2017-09-08 15:16:59 -0300

    a

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var app = express();
 var execSync = require('child_process').execSync;
 
 app.get('/webhook', function (req, res) {
-  var result = execSync("git rev-list --left-right --count origin/master...origin/develop").toString()
-  var behind = parseInt(result.split(" ")[0]) > 0;
-  res.send(behind)
+  //var result = execSync("git rev-list --left-right --count origin/master...origin/develop").toString()
+  //var behind = parseInt(result.split(" ")[0]) > 0;
+  var result = execSync("git branch -v").toString()
+  result += execSync("git remote -v ").toString()
+  res.send(result)
 });
 
 app.listen(process.env.PORT || 3000, function () {
