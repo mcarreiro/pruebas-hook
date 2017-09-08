fb87da522af3b9c9def3d98bb7f6389c8efe7f3b
Martin Carreiro <martin.carreiro@mercadolibre.com>
2017-09-08 15:07:23 -0300

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 var execSync = require('child_process').execSync;
 
-app.get('/', function (req, res) {
+app.get('/webhook', function (req, res) {
   var result = execSync("git rev-list --left-right --count origin/master...origin/develop").toString()
   var behind = parseInt(result.split(" ")[0]) > 0;
   res.send(behind)
