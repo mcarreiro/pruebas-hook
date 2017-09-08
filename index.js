431789c4ec8de54fd29ba784c5e4e3852b761174
Martin Carreiro <martin.carreiro@mercadolibre.com>
2017-09-08 15:03:00 -0300

    add

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var execSync = require('child_process').execSync;
+
+app.get('/', function (req, res) {
+  var result = execSync("git rev-list --left-right --count origin/master...origin/develop").toString()
+  var behind = parseInt(result.split(" ")[0]) > 0;
+  res.send(behind)
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!');
+});
